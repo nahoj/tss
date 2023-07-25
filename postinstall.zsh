0447b22a70f90f67b5f4936d17c3e2d3bffc105e
Johan Grande <nahoj+git@crans.org>
2023-07-25 07:58:00 +0200

    add common tag suggestions to .zshrc

--- postinstall.zsh
+++ postinstall.zsh
@@ -17,7 +17,23 @@ EOF
 fi
 
 local zshrc=${ZDOTDIR:-$HOME}/.zshrc
+
 if ! grep -q 'autoload -U tss' $zshrc; then
   print -r "Adding 'autoload -U tss' to ${(q-)zshrc}."
   print '\nautoload -U tss' >>$zshrc
+else
+  print -r "'autoload -U tss' already present in ${(q-)zshrc}."
+fi
+
+if ! grep -q 'tss_tags_' $zshrc; then
+  print -r "Adding common tag suggestions to ${(q-)zshrc}. You may want to edit them."
+  {
+    print -r '# Array variables named tss_tags_* are added to tag suggestions for tss add'
+    print -r 'local tss_tags_ratings=(1star 2star 3star 4star 5star)'
+    print -r 'local tss_tags_media=(toread reading read towatch watched)'
+    print -r 'local tss_tags_workflow=(todo draft done published)'
+    print -r 'local tss_tags_life=(family friends personal school vacation work other)'
+  } >>$zshrc
+else
+  print -r "Common tag suggestions already present in ${(q-)zshrc}."
 fi
